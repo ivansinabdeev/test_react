61661b5c500b24c618b43f5e21145905afd26896
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2024-03-27 00:34:55 +0200

    build ?

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import glob from "glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      input: glob.sync("./src/*.html"),
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-        entryFileNames: "commonHelpers.js",
-      },
-    },
-    outDir: "../dist",
   },
 });
